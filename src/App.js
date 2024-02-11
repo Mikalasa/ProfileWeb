6a1d2e8bbcad0f66678a77e2d7c09f84f8ece8df
gezhe <htt3001ptq@hotmail.com>
2024-02-12 01:25:57 +1300

    change back to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import StarsCanvas from "./component/canvas/StarsBG";
 import Navbar from "./component/common/Navbar";
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import TheThinkerCanvas from "./component/canvas/TheThinker";
 import Projects from "./component/layout/Projects";
 import HeroInfo from "./component/content/HeroInfo";
@@ -20,7 +20,7 @@ function App() {
     }, []);
 
     return (
-        <HashRouter>
+        <BrowserRouter>
             <div className="App">
                 <Navbar/>
                 <div className="hero-container layout-container">
@@ -38,7 +38,7 @@ function App() {
                     <WebglPc/>
                 </div>
             </div>
-        </HashRouter>
+        </BrowserRouter>
     );
 }
 
