4cac8e1e34221788f3309cbcadf34d226805d5a3
gezhe <htt3001ptq@hotmail.com>
2024-10-06 15:29:28 +1300

    update pc animation

--- src/component/canvas/WebglPc.jsx
+++ src/component/canvas/WebglPc.jsx
@@ -1,9 +1,9 @@
-import React, {Suspense, useEffect, useMemo, useRef, useState} from 'react';
+import React, { Suspense, useEffect, useMemo, useRef, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-import {Html, Preload, PresentationControls, useGLTF} from '@react-three/drei';
+import { Html, Preload, PresentationControls, useGLTF } from '@react-three/drei';
 import CanvasLoader from "./CanvasLoader";
-import {CanvasTexture, LinearFilter} from "three";
-
+import { CanvasTexture, LinearFilter } from "three";
+import { useSpring, animated } from '@react-spring/three';
 
 function useOperatingSystem() {
     const [os, setOs] = useState('');
@@ -12,23 +12,35 @@ function useOperatingSystem() {
         const userAgent = window.navigator.userAgent;
         if (userAgent.includes('Win')) setOs('windows');
         if (userAgent.includes('Mac')) setOs('mac');
-        // Add more conditions if you want to detect other operating systems.
     }, []);
 
     return os;
 }
 
-
-const MacModel = ({startAnimation}) => {
+const MacModel = ({ startAnimation }) => {
     const scene = useGLTF("./pc.glb");
     const dirLightRef = useRef();
     const os = useOperatingSystem();
+
+    const { position: modelPosition, rotation: modelRotation } = useSpring({
+        from: { position: [-10, -2.8, -15], rotation: [0, Math.PI / 2, 0] },
+        to: { position: [3, -2.8, 5.5], rotation: [0, - Math.PI / 2, 0] },
+        config: { tension: 40, friction: 15 },
+        reset: startAnimation,
+    });
+
+    // compute relative position of iframe
+    const { position: iframePosition } = useSpring({
+        from: { position: [-10 - 3, -2.8 + 3, -15 - 1.2] },
+        to: { position: [3 - 3, -2.8 + 3, 5.5 - 1.2] },
+        config: { tension: 40, friction: 15 },
+        reset: startAnimation,
+    });
 
     useEffect(() => {
         if (scene) {
             scene.scene.traverse((child) => {
                 if (child.isMesh) {
-                    // console.log(child.material.map.source)
                     child.castShadow = true;
                     child.receiveShadow = true;
                 }
@@ -38,7 +50,7 @@ const MacModel = ({startAnimation}) => {
 
     return (
         <>
-            <ambientLight intensity={2} color={"#ffffff"}/>
+            <ambientLight intensity={2} color={"#ffffff"} />
             <directionalLight
                 ref={dirLightRef}
                 intensity={0.3}
@@ -49,30 +61,36 @@ const MacModel = ({startAnimation}) => {
                 shadow-radius={10}
             />
             <pointLight position={[4, 0, 8]} intensity={50} color={"#7cb3c4"} castShadow shadow-mapSize-width={512}
-                        shadow-mapSize-height={512} shadow-radius={10}/>
+                shadow-mapSize-height={512} shadow-radius={10} />
             <pointLight position={[-7, 1, 8]} intensity={100} color={"#d24e4e"} castShadow shadow-mapSize-width={512}
-                        shadow-mapSize-height={512} shadow-radius={10}/>
+                shadow-mapSize-height={512} shadow-radius={10} />
             <pointLight position={[2, 1, -8]} intensity={50} color={"#a308ef"} castShadow shadow-mapSize-width={512}
-                        shadow-mapSize-height={512} shadow-radius={10}/>
-            <primitive
-                object={scene.scene} scale={1} position={[3, -2.8, 5.5]}
-                rotation={[0, -Math.PI / 2, 0]}
-            />
-            <Html
-                position={[0, 0.2, 4.3]}
-                transform occlude
-                wrapperClass="webgl-iframe-wrapper"
-                distanceFactor={1.65}
-                rotation-x={(-4.55 * Math.PI) / 180}
-            >
-                <iframe
-                    className={`webgl-iframe webgl-iframe-${os}`}
-                    src="https://mikalasa.github.io/ProfileWeb-Iframe-About/"
+                shadow-mapSize-height={512} shadow-radius={10} />
+
+            <animated.group position={modelPosition} rotation={modelRotation}>
+                <primitive
+                    object={scene.scene}
+                    scale={1}
                 />
-            </Html>
+            </animated.group>
+
+            <animated.group position={iframePosition}>
+                <Html
+                    transform
+                    occlude
+                    wrapperClass="webgl-iframe-wrapper"
+                    distanceFactor={1.65}
+                >
+                    <iframe
+                        className={`webgl-iframe webgl-iframe-${os}`}
+                        src="https://mikalasa.github.io/ProfileWeb-Iframe-About/"
+                    />
+                </Html>
+            </animated.group>
         </>
     );
 };
+
 
 const Wall = () => {
     const gradientTexture = useMemo(() => {
@@ -97,7 +115,7 @@ const Wall = () => {
             receiveShadow
             castShadow
         >
-            <planeGeometry args={[200, 200]}/>
+            <planeGeometry args={[200, 200]} />
             <meshPhysicalMaterial
                 map={gradientTexture}
                 metalness={0.5}
@@ -107,24 +125,57 @@ const Wall = () => {
     );
 };
 
-
 function WebglPc() {
+    const [startAnimation, setStartAnimation] = useState(false);
+    const sectionRef = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        setStartAnimation(true);
+
+                        const currentScrollY = window.scrollY;  // current scroll position
+                        const sectionTop = sectionRef.current.getBoundingClientRect().top + currentScrollY;  // active section top
+
+                        window.scrollTo({
+                            top: sectionTop,
+                            behavior: 'smooth',
+                        });
+                    }
+                });
+            },
+            { threshold: 0.05 }
+        );
+
+        if (sectionRef.current) {
+            observer.observe(sectionRef.current);
+        }
+
+        return () => {
+            if (sectionRef.current) {
+                observer.unobserve(sectionRef.current);
+            }
+        };
+    }, []);
+
     return (
-        <div className="mac-bg">
+        <div className="mac-bg" ref={sectionRef}>
             <Canvas
                 shadows
                 dpr={[1, 2]}
-                camera={{position: [0, 0, 15], fov: 25, near: 0.1, far: 200}}
-                gl={{antialias: true, alpha: false, preserveDrawingBuffer: true }}
+                camera={{ position: [0, 0, 15], fov: 25, near: 0.1, far: 200 }}
+                gl={{ antialias: true, alpha: false, preserveDrawingBuffer: true }}
             >
                 <Suspense fallback={<CanvasLoader />}>
                     <PresentationControls
                         snap={true}
-                        config={{ tension: 170, friction: 26 }}
+                        config={{ tension: 120, friction: 20 }}
                         azimuth={[(-20 * Math.PI) / 180, (20 * Math.PI) / 180]}
                         polar={[(-5 * Math.PI) / 180, (15 * Math.PI) / 180]}
                     >
-                        <MacModel/>
+                        <MacModel startAnimation={startAnimation} />
                         <Wall />
                     </PresentationControls>
                 </Suspense>
@@ -133,6 +184,5 @@ function WebglPc() {
         </div>
     );
 }
-
 
 export default WebglPc;
