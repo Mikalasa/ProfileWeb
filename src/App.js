c1c5e41049e625d1d2c12725d409fb4e6d47f5e0
gezhe <44009421+Mikalasa@users.noreply.github.com>
2024-12-06 01:51:52 +1300

    re add mobile warning modal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { lazy, Suspense, useEffect, useState } from "react";
+import { isMobile } from "react-device-detect";
 import Navbar from "./component/widgets/Navbar.jsx";
+import MobileWarningModal from "./component/modal/MobileWarningModal.jsx"; // 引入 MobileWarningModal
 import { AutoScrollContext } from './utility/AutoScrollContext';
 import withAutoScroll from "./customHooks/withAutoScroll.jsx";
-import HeroCard from "./component/widgets/HeroCard";
 
 const Hero = lazy(() => import("./component/layout/Hero.jsx"));
 const ParticleTextWrapper = lazy(() => import("./component/layout/ParticleTextWrapper.jsx"));
@@ -18,18 +19,30 @@ const AutoScrollAbout = withAutoScroll(About);
 
 function App() {
     const [isNavClick, setIsNavClick] = useState(false);
+    const [showMobileWarning, setShowMobileWarning] = useState(false); // 控制模态框显示状态
 
     useEffect(() => {
         if ('scrollRestoration' in window.history) {
             window.history.scrollRestoration = 'manual';
         }
         window.scrollTo(0, 0);
+
+        if (isMobile) {
+            setShowMobileWarning(true);
+        }
     }, []);
 
     return (
         <AutoScrollContext.Provider value={{ isNavClick, setIsNavClick }}>
             <div className="App">
                 <Navbar />
+
+                {/* MobileWarningModal */}
+                {showMobileWarning && (
+                    <MobileWarningModal onClose={() => setShowMobileWarning(false)} />
+                )}
+
+                {/* Suspense */}
                 <Suspense fallback={<div>Loading...</div>}>
                     <AutoScrollHero />
                     <AutoScrollParticleText text="Welcome to my profile website！🚀" />
